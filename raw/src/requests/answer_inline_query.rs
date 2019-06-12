f47dfe942be4f9c12f3752a378faabe3aa8c988d
kekonen <daniil.naumetc@gmail.com>
2019-06-12 23:22:18 +0200

    added switch_pm_text to inline answer

--- raw/src/requests/answer_inline_query.rs
+++ raw/src/requests/answer_inline_query.rs
@@ -4,7 +4,18 @@ use crate::types::*;
 #[derive(Serialize, Debug)]
 pub struct AnswerInlineQuery {
     inline_query_id: InlineQueryId,
+
+    #[serde(skip_serializing_if = "Vec::is_empty")]
     results: Vec<InlineQueryResult>,
+
+    is_personal: bool,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    switch_pm_text: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    switch_pm_parameter: Option<String>,
+    
     // TODO: Rest of the fields
 }
 
@@ -18,15 +29,15 @@ impl Request for AnswerInlineQuery {
 }
 
 pub trait CanAnswerInlineQuery {
-    fn answer(self, results: Vec<InlineQueryResult>) -> AnswerInlineQuery;
+    fn answer(self, results: Vec<InlineQueryResult>, switch_pm_text: Option<String>, switch_pm_parameter: Option<String>) -> AnswerInlineQuery;
 }
 
 impl<T> CanAnswerInlineQuery for T
 where
     T: Into<InlineQueryId>,
 {
-    fn answer(self, results: Vec<InlineQueryResult>) -> AnswerInlineQuery {
-        AnswerInlineQuery::new(self.into(), results)
+    fn answer(self, results: Vec<InlineQueryResult>, switch_pm_text: Option<String>, switch_pm_parameter: Option<String>) -> AnswerInlineQuery {
+        AnswerInlineQuery::new(self.into(), results, switch_pm_text, switch_pm_parameter)
     }
 }
 
@@ -34,10 +45,15 @@ impl AnswerInlineQuery {
     pub fn new(
         inline_query_id: InlineQueryId,
         results: Vec<InlineQueryResult>,
+        switch_pm_text: Option<String>,
+        switch_pm_parameter: Option<String>,
     ) -> AnswerInlineQuery {
         AnswerInlineQuery {
             inline_query_id,
             results,
+            is_personal: true,
+            switch_pm_text,
+            switch_pm_parameter,
         }
     }
 
